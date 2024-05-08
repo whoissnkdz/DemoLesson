b0a11878883b47e5cb673e653f7e6489ea4c8e9e
SNKDZ <kovalenko74.12@icloud.com>
2024-05-08 15:58:07 +0200

    lesson2

---
+++ lesson_2.py
@@ -0,0 +1,94 @@
+#int - ціле число 12
+#float - дробове число 12.5
+#bool - логічний тип даних: true/ false
+#str - рядок/ масив (набір) символів
+
+#змінна - це іменована область ОЗУ значення якої можно змінювати
+# number: int = 10
+# print (number)
+# print (type(number))
+# number = 20.5
+# print (number)
+# print (type(number))
+# number = 'Text1'
+# print (number)
+# print (type(number))
+# number = True
+# print (number)
+# print (type(number))
+
+###
+# print (number := 10)  # моржовый оператор
+###
+# userName1 = "Vasya"
+# print (userName1)
+# user_name = "Misha"
+# print (user_name)
+###
+
+########
+# + - * /
+# print (2 + 3)
+# print (2 - 3)
+# print (2 * 3)
+# print (2 / 3)
+# print(2**3) #основа ** показник (зведення в ступінь)
+# print(2 // 3) #ціла частина від розподілу
+# print(2 % 3) #залишок від ділення
+
+########
+
+
+# number = int(input("Enter a number: "))
+# n1 = number // 100
+# # v1
+# # v2 = number // 10 % 10
+# # v2
+# n2 = number % 100 // 10
+# n3 = number % 10
+#
+# print(n1)
+# print(n2)
+# print(n3)
+#
+# result = n1 + n2 + n3
+# print("Result:", result)
+
+###
+# input
+#
+
+# name = (input('ВВедите имя: '))
+# age = int(input('ВВедите возраст: '))
+#
+# # v1
+# print('Привет, ', name, 'тебе ', age, 'лет!')
+#
+# # v2 коннкатенація - складення рядків. Рядок - рядок - один великий рядок
+# print('Hello, ' + name + ' you are ' + str(age) + ' years old!')
+#
+# # v3 інтерполяція - вбудовування змінних у рядок завдяки ф-ції format
+# print(f'Hallo, {name}. Du bist {age} jahre alt!')
+
+# age_after_ten_years = age + 10
+# print (age_after_ten_years)
+
+#Пользователь вводит с клавиатуры три числа. Необходимо найти сумму чисел, произведение чисел
+#Результат вычеслений вывести на экран
+
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+# number3 = int(input("Enter third number: "))
+#
+# numbers_sum = number1 + number2 + number3
+# numbers_multiplication = number1 * number2 * number3
+# print(f"The sum of the numbers: {numbers_sum}")
+# print(f"The multiplicate of the numbers: {numbers_multiplication}")
+
+# Напишите программу, вычисляющую площадь ромба
+
+# first_diagonal = int(input("Enter first diagonal: "))
+# second_diagonal = int(input("Enter second diagonal: "))
+# area = first_diagonal* second_diagonal / 2
+# print(f"The area of the rhombus: {area}")
+zxc
